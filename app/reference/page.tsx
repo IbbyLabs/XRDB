3059a0dcf5a5b1fdd60325f32a6b32a082e2a8aa
IbbyLabs <40578997+IbbyLabs@users.noreply.github.com>
2026-06-10 02:29:28 +0100

    chore(v3): remove v2 Next.js app routes

--- app/reference/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ReferenceView } from '@/components/reference-view';
-
-export default function ReferencePage() {
-  return (
-    <div className="xrdb-page min-h-screen bg-transparent">
-      <ReferenceView />
-    </div>
-  );
-}
